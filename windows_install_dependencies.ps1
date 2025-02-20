202b9f6e404b77c49b9c6d2165a905ce5eedb1d7
Wolfgang Betz <w.betz@fesslix.org>
2025-02-20 15:11:15 +0100

    Windows-build: install full boost

--- windows_install_dependencies.ps1
+++ windows_install_dependencies.ps1
@@ -3,7 +3,7 @@ git clone https://github.com/microsoft/vcpkg.git
 Write-Output 'Bootstrapping vcpkg...'
 .\vcpkg\bootstrap-vcpkg.bat
 Write-Output 'Installing GSL and Boost...'
-.\vcpkg\vcpkg install gsl boost-header
+.\vcpkg\vcpkg install gsl boost
 Write-Output 'Setting environment variables...'
 [System.Environment]::SetEnvironmentVariable('VCPKG_ROOT', "$PWD\vcpkg", [System.EnvironmentVariableTarget]::Process)
 [System.Environment]::SetEnvironmentVariable('CMAKE_TOOLCHAIN_FILE', "$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake", [System.EnvironmentVariableTarget]::Process)

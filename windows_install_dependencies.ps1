63ed1ccc17eaea5258a0375e64a6d2164d53e615
Wolfgang Betz <w.betz@fesslix.org>
2025-02-20 15:53:01 +0100

    Windows-build: use run-vcpkdg

--- windows_install_dependencies.ps1
+++ windows_install_dependencies.ps1
@@ -1,13 +1,13 @@
-Write-Output 'Cloning vcpkg repository...'
-git clone https://github.com/microsoft/vcpkg.git
-Write-Output 'Bootstrapping vcpkg...'
-.\vcpkg\bootstrap-vcpkg.bat
-Write-Output 'Installing GSL and Boost...'
-.\vcpkg\vcpkg install gsl boost
-Write-Output 'Setting environment variables...'
-[System.Environment]::SetEnvironmentVariable('VCPKG_ROOT', "$PWD\vcpkg", [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('CMAKE_TOOLCHAIN_FILE', "$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake", [System.EnvironmentVariableTarget]::Process)
-Write-Output 'Setup completed successfully.'
+#Write-Output 'Cloning vcpkg repository...'
+#git clone https://github.com/microsoft/vcpkg.git
+#Write-Output 'Bootstrapping vcpkg...'
+#.\vcpkg\bootstrap-vcpkg.bat
+#Write-Output 'Installing GSL and Boost...'
+#.\vcpkg\vcpkg install gsl boost
+#Write-Output 'Setting environment variables...'
+#[System.Environment]::SetEnvironmentVariable('VCPKG_ROOT', "$PWD\vcpkg", [System.EnvironmentVariableTarget]::Process)
+#[System.Environment]::SetEnvironmentVariable('CMAKE_TOOLCHAIN_FILE', "$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake", [System.EnvironmentVariableTarget]::Process)
+#Write-Output 'Setup completed successfully.'
 Write-Output 'Check if pkg-config Can Find GSL ...'
 vcpkg list | Select-String gsl
 vcpkg integrate install

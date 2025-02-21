54a611a06f64a9dd5bd0790c3388b44e500e76d8
Wolfgang Betz <w.betz@fesslix.org>
2025-02-21 07:48:20 +0100

    Windows-build: set environment variable

--- windows_install_dependencies.ps1
+++ windows_install_dependencies.ps1
@@ -4,19 +4,24 @@
 #.\vcpkg\bootstrap-vcpkg.bat
 #Write-Output 'Installing GSL and Boost...'
 #.\vcpkg\vcpkg install gsl boost
-#Write-Output 'Setting environment variables...'
-#[System.Environment]::SetEnvironmentVariable('VCPKG_ROOT', "$PWD\vcpkg", [System.EnvironmentVariableTarget]::Process)
-#[System.Environment]::SetEnvironmentVariable('CMAKE_TOOLCHAIN_FILE', "$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake", [System.EnvironmentVariableTarget]::Process)
 #Write-Output 'Setup completed successfully.'
 Write-Output 'VCPKG update an version'
 vcpkg version
 vcpkg update
 vcpkg upgrade --no-dry-run
 vcpkg version
+
 Write-Output 'Installing dependencies ...'
 vcpkg install --triplet x64-windows
+
 Write-Output 'Check if pkg-config Can Find GSL ...'
 vcpkg list | Select-String gsl
 vcpkg integrate install
 pkg-config --cflags gsl
 pkg-config --libs gsl
+
+Write-Output 'Setting environment variables...'
+[System.Environment]::SetEnvironmentVariable('VCPKG_ROOT', "$PWD\vcpkg", [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('CMAKE_TOOLCHAIN_FILE', "$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake", [System.EnvironmentVariableTarget]::Process)
+Write-Output "20250221: $PWD\vcpkg\scripts\buildsystems\vcpkg.cmake"
+

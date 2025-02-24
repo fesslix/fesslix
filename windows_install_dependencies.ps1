960c0b47adc9fdf342456a37db80139d58c776f0
Wolfgang Betz <w.betz@fesslix.org>
2025-02-24 08:26:10 +0100

    Windows-build: manual vcpkg - v27

--- windows_install_dependencies.ps1
+++ windows_install_dependencies.ps1
@@ -1,42 +1,21 @@
 Write-Output 'Cloning vcpkg repository...'
-#git clone https://github.com/Microsoft/vcpkg.git
-#.\vcpkg\bootstrap-vcpkg.bat
-vcpkg integrate install
+git clone https://github.com/Microsoft/vcpkg.git
+.\vcpkg\bootstrap-vcpkg.bat
+.\vcpkg\vcpkg integrate install
 
 Write-Output 'Installing dependencies ...'
-vcpkg install gsl gsl:x86-windows gsl:arm64-windows boost-format boost-math boost-concept-check boost-random
+.\vcpkg\vcpkg install gsl gsl:x86-windows gsl:arm64-windows boost-format boost-math boost-concept-check boost-random
 
-vcpkg integrate install
-
-#git clone https://github.com/microsoft/vcpkg.git
-#Write-Output 'Bootstrapping vcpkg...'
-#.\vcpkg\bootstrap-vcpkg.bat
-#Write-Output 'Installing GSL and Boost...'
-#.\vcpkg\vcpkg install gsl boost
-#Write-Output 'Setup completed successfully.'
-
-#Write-Output 'VCPKG update an version'
-#vcpkg version
-#vcpkg update
-#vcpkg upgrade --no-dry-run
-#vcpkg version
-
-#Write-Output 'Installing dependencies ...'
-#vcpkg install --triplet x64-windows
+.\vcpkg\vcpkg integrate install
 
 Write-Output 'Check if pkg-config Can Find GSL ...'
-vcpkg list | Select-String gsl
-vcpkg list | Select-String boost
-$env:PKG_CONFIG_PATH="C:\vcpkg\installed\x64-windows\lib\pkgconfig"
+.\vcpkg\vcpkg list | Select-String gsl
+.\vcpkg\vcpkg list | Select-String boost
+$env:PKG_CONFIG_PATH="D:\a\fesslix\fesslix\vcpkg\installed\x64-windows\lib\pkgconfig"
 Write-Output '   pkg-config ...'
 pkg-config --cflags gsl
 pkg-config --libs gsl
 pkg-config --modversion gsl
 
-#Write-Output 'Setting environment variables...'
-#[System.Environment]::SetEnvironmentVariable('VCPKG_ROOT', "$PWD\vcpkg", [System.EnvironmentVariableTarget]::Process)
-#[System.Environment]::SetEnvironmentVariable('CMAKE_TOOLCHAIN_FILE', "$PWD\vcpkg\scripts\buildsystems\vcpkg.cmake", [System.EnvironmentVariableTarget]::Process)
-#Write-Output "20250221: $CMAKE_TOOLCHAIN_FILE"
-
 Write-Output 'List files in directory'
-#Get-ChildItem -Path "C:\vcpkg" -Recurse -File | Select-Object -ExpandProperty FullName
+#Get-ChildItem -Path "D:\a\fesslix\fesslix\vcpkg" -Recurse -File | Select-Object -ExpandProperty FullName

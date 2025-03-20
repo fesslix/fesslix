d56792713b6f73813b4999c09948a0c19a865287
Wolfgang Betz <w.betz@fesslix.org>
2025-03-20 08:45:15 +0100

    attempt package build

--- windows_install_dependencies.ps1
+++ windows_install_dependencies.ps1
@@ -4,7 +4,7 @@ git clone https://github.com/Microsoft/vcpkg.git
 .\vcpkg\vcpkg integrate install
 
 Write-Output 'Installing dependencies ...'
-.\vcpkg\vcpkg install gsl-static gsl:x86-windows-static gsl:arm64-windows-static boost-format boost-math boost-concept-check boost-random boost-algorithm
+.\vcpkg\vcpkg install gsl-static gsl:x86-windows-static gsl:arm64-windows-static boost-format-static boost-math-static boost-concept-check-static boost-random-static boost-algorithm-static
 
 .\vcpkg\vcpkg integrate install
 
@@ -19,5 +19,6 @@ pkg-config --modversion gsl
 
 Write-Output 'List files in directory'
 #Get-ChildItem -Path "D:\a\fesslix\fesslix\vcpkg" -Recurse -File | Select-Object -ExpandProperty FullName
-dumpbin /symbols D:\a\fesslix\fesslix\vcpkg\installed\x86-windows-static\lib\gsl.lib
+#dumpbin /symbols D:\a\fesslix\fesslix\vcpkg\installed\x86-windows-static\lib\gsl.lib
+llvm-readobj --exports D:\a\fesslix\fesslix\vcpkg\installed\x86-windows-static\lib\gsl.lib
 

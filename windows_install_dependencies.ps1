938ff558a5ae5f024094a990f78e3b259d782eb2
Wolfgang Betz <w.betz@fesslix.org>
2025-03-20 08:30:59 +0100

    attempt package buidl

--- windows_install_dependencies.ps1
+++ windows_install_dependencies.ps1
@@ -4,14 +4,14 @@ git clone https://github.com/Microsoft/vcpkg.git
 .\vcpkg\vcpkg integrate install
 
 Write-Output 'Installing dependencies ...'
-.\vcpkg\vcpkg install gsl gsl:x86-windows-static gsl:arm64-windows-static boost-format boost-math boost-concept-check boost-random boost-algorithm
+.\vcpkg\vcpkg install gsl-static gsl:x86-windows-static gsl:arm64-windows-static boost-format boost-math boost-concept-check boost-random boost-algorithm
 
 .\vcpkg\vcpkg integrate install
 
 Write-Output 'Check if pkg-config Can Find GSL ...'
 .\vcpkg\vcpkg list | Select-String gsl
 .\vcpkg\vcpkg list | Select-String boost
-$env:PKG_CONFIG_PATH="D:\a\fesslix\fesslix\vcpkg\installed\x64-windows\lib\pkgconfig"
+$env:PKG_CONFIG_PATH="D:\a\fesslix\fesslix\vcpkg\installed\x64-windows-static\lib\pkgconfig"
 Write-Output '   pkg-config ...'
 pkg-config --cflags gsl
 pkg-config --libs gsl
@@ -19,3 +19,5 @@ pkg-config --modversion gsl
 
 Write-Output 'List files in directory'
 #Get-ChildItem -Path "D:\a\fesslix\fesslix\vcpkg" -Recurse -File | Select-Object -ExpandProperty FullName
+dumpbin /symbols D:\a\fesslix\fesslix\vcpkg\installed\x86-windows-static\lib\gsl.lib
+

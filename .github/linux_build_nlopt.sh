180cfd7d8f38dfdaf3861d99dd60aa6b8ab14329
Wolfgang Betz <w.betz@fesslix.org>
2025-07-23 08:25:00 +0200

    attempt package build » linking against tbb

--- .github/linux_build_nlopt.sh
+++ .github/linux_build_nlopt.sh
@@ -9,3 +9,10 @@ cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/../install
 make -j
 make install
 echo $PWD
+echo "========================="
+echo "TBB » build from source"
+echo "========================="
+git clone https://github.com/oneapi-src/oneTBB.git
+cd oneTBB
+cmake -B build -DCMAKE_INSTALL_PREFIX=$PWD/../install
+cmake --build build --target install

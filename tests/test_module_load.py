67ab0292838994f5681da45816c3e67dd0dd57fd
Wolfgang Betz <w.betz@fesslix.org>
2025-07-23 10:52:01 +0200

    add first test script

---
+++ tests/test_module_load.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Python script to test if the Fesslix module can be imported
+
+Copyright (C) 2010-2025 Wolfgang Betz
+
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved.  This file is offered as-is,
+without any warranty.
+"""
+
+import fesslix as flx
+print(flx.__version__)
+flx.load_engine()
+
+import fesslix.gpr
+import fesslix.tools
+import fesslix.model_templates
+
+
+import numpy as np
+
+flx.print_info()
+
